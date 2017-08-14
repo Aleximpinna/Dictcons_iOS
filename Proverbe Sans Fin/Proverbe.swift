23b298a70020cf3ff16e655794c62d4970ead4b3
Alexandre Impinna <alexandreimpinna@mbpdealexandre.local>
2017-08-14 09:15:22 -0400

    Initial commit

---
+++ Proverbe Sans Fin/Proverbe.swift
@@ -0,0 +1,16 @@
+//
+//  Proverbe.swift
+//  Proverbe Sans Fin
+//
+//  Created by Alexandre Impinna on 02/08/2017.
+//  Copyright © 2017 Alexandre Impinna. All rights reserved.
+//
+
+import Foundation
+
+class Proverbe {
+    
+    var proverbe: [String] = ["Il ne faut pas vendre la peau de l'ours", "On n'apprend pas aux vieux singes", "Il faut prendre le taureau", "Il ne faut pas se moquer des chiens", "Il ne faut pas péter plus haut", "Il vaut mieux avoir affaire à Dieu", "Il ne faut pas prendre les enfants", "Les bons comptes", "Loin des yeux", "L'amour nait d'un regard", "On ne peut avoir le beurre", "La vengeance est un plat", "Mieux vaut sagesse", "Si jeunesse savait", "Les voyages forment"]
+    
+}
+
--- Proverbe Sans Fin/ViewController.swift
+++ Proverbe Sans Fin/ViewController.swift
@@ -7,18 +7,82 @@
 //
 
 import UIKit
+import Hue
+
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet var swipeCredits: UISwipeGestureRecognizer!
+    @IBOutlet var changeProverbeTapGesture: UITapGestureRecognizer!
+    @IBOutlet weak var proverbeLabel: UILabel!
+    
+    var numeroProverbe = 0
+    let proverbes = Proverbe()
+    var proverbeLayer = CALayer()
+    var layerNumber = 0
+    
+
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad() 
+        
+        
+    }
+    
+    @IBAction func swipeToCredits(_ sender: UISwipeGestureRecognizer) {
+    
+    performSegue(withIdentifier: "SegueToCredits", sender: (Any).self)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func changeProverbeAction(_ sender: UITapGestureRecognizer) {
+        
+        randomBackground()
+        randomProverbe()
+
     }
+    
+    func randomProverbe() {
+        
+        numeroProverbe = Int(arc4random_uniform(UInt32(proverbes.proverbe.count)))
+        proverbeLabel.text = proverbes.proverbe[numeroProverbe]
+        
+    }
+    
+    func randomBackground() {
+        
+        var random1 = CGFloat(randomFloat(min: 0.0, max: 1))
+        var random2 = CGFloat(randomFloat(min: 0.0, max: 1))
+        var random3 = CGFloat(randomFloat(min: 0.0, max: 1))
+        var random4 = CGFloat(randomFloat(min: 0.0, max: 1))
+        var random5 = CGFloat(randomFloat(min: 0.0, max: 1))
+        var random6 = CGFloat(randomFloat(min: 0.0, max: 1))
+        
+        // Détermine la couleur de la cellule
+        let colorTop = UIColor(red: CGFloat(random1), green: CGFloat(random2), blue: CGFloat(random3), alpha: 1.0)
+        let colorDown = UIColor(red: CGFloat(random4), green: CGFloat(random5), blue: CGFloat(random6), alpha: 1.0)
+        
+        let gradient = [colorTop, colorDown].gradient { gradient in
+            gradient.locations = [0.0, 1.0]
+            return gradient
+        }
+        
+        gradient.frame = self.view.bounds
+        
+        if layerNumber == 0 {
+            view.layer.insertSublayer(gradient, at: 0)
+            layerNumber += 1
+        } else if layerNumber == 1 {
+            view.layer.replaceSublayer((view.layer.sublayers?[0])!, with: gradient)
+        }
+        
+    }
+    
+    func randomFloat(min: Float, max: Float) -> Float {
+        return (Float(arc4random()) / 0xFFFFFFFF) * (max - min) + min
+    }
+    
 
 
 }
